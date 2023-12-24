7d1cd6f6306abbf61699a9a5fb3a3177341a6456
dev4thedeveloper <145423867+dev4thedeveloper@users.noreply.github.com>
2023-12-25 01:20:59 +0530

    Completed, 2 errors left

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import './App.css';
+
+const accessKey = "5BvZ6IsmQvXod2e1aX094xdpu5KTCuE4nVKOQ_bGwzA";
+
+const ImageSearchApp = () => {
+  const [keyword, setKeyword] = useState("");
+  const [page, setPage] = useState(1);
+  const [results, setResults] = useState([]);
+  const [showMoreBtnDisplay, setShowMoreBtnDisplay] = useState("none");
+
+  const searchImage = async () => {
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+    const response = await fetch(url);
+    const data = await response.json();
+    if (page === 1) {
+      setResults(data.results);
+    } else {
+      setResults((prevResults) => [...prevResults, ...data.results]);
+    }
+    setShowMoreBtnDisplay("block");
+  };
+
+  useEffect(() => {
+    searchImage();
+  }, [page, keyword]);  
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setPage(1);
+    searchImage();
+  };
+
+  const handleShowMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  return (
+    <div>
+      <h1>image search engine</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={keyword}
+          placeholder="Search something...."
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div id="search-result">
+        {results.map((result, index) => (
+          <img
+            key={index}
+            src={result.urls.small}
+            alt={result.alt_description}
+          />
+        ))}
+      </div>
+      <button
+        id="show-more-btn"
+        style={{ display: showMoreBtnDisplay }}
+        onClick={handleShowMore}
+      >
+        Show More
+      </button>
+    </div>
+  );
+};
+
+export default ImageSearchApp;
